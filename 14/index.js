a6fc13f81664360caca45e19a7d5efa8eb929207
nanerseasac <renan.casaeses@gmail.com>
2023-07-18 12:44:10 -0300

    DONE

--- 14/index.js
+++ 14/index.js
@@ -1,13 +1,9 @@
 
-const arrayA = [1, 3, 3, 44, 11];
-const arrayB = [57, 4, 21, 32, 13];
+const arrayA = [5, 32, 3, 44, 1];
+const arrayB = [57, 4, 21, 2, 13];
 
 
 
 for(let i = 0;i < 5; i++) {
-	if(arrayA[i] > arrayB[i]) {
-		console.log(arrayB[i])
-	}else if(arrayB[i] > arrayA[i]) {
-		console.log(arrayA[i])
-	}
+	arrayA[i] > arrayB[i] ? console.log(arrayB[i]) : console.log(arrayA[i])
 }
